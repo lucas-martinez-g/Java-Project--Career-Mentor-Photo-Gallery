b771bc18d7cbd08e966f66e0d592a6e1f70f2c6e
lucas-martinez-g <lucas.martinez01@globant.com>
2024-03-20 15:23:44 -0300

    create proyect

---
+++ src/main/java/lucas_m/career_mentor/deliverable_proyect/DeliverableProyectApplication.java
@@ -0,0 +1,13 @@
+package lucas_m.career_mentor.deliverable_proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverableProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverableProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lucas_m/career_mentor/deliverable_proyect/DeliverableProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package lucas_m.career_mentor.deliverable_proyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverableProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
